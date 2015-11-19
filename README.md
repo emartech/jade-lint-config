ed566899a095066d37fc108837f08378349a9cae
József Kozma <jozsef.kozma@gmail.com>
2015-11-19 09:55:31 +0100

    chore(init): initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# jade-lint-config
+
+## Usage
+
+```sh
+$ npm i -D gulp-jade-lint jade-lint-config-emarsys
+```
+
+Then add it to your .jade-lintrc
+
+```
+{
+  "extends": [
+    "emarsys"
+  ],
+  "someSetting": true,
+  "someOtherSetting": true
+}
+```
---
+++ index.js
@@ -0,0 +1,11 @@
+module.exports = {
+  "disallowClassLiteralsBeforeIdLiterals": true,
+  "disallowDuplicateAttributes": true,
+  "disallowMultipleLineBreaks": true,
+  "disallowSpacesInsideAttributeBrackets": true,
+  "requireClassLiteralsBeforeAttributes": true,
+  "requireLowerCaseAttributes": true,
+  "requireLowerCaseTags": true,
+  "validateDivTags": true,
+  "validateIndentation": 2
+};
